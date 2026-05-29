1d2bb9f4a1a47db0be99e9e217e642caa3732065
Aghazie David <aghaziedavid@gmail.com>
2026-05-29 06:22:04 +0100

    Fix chunk size warning for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5174,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
   }
 })
